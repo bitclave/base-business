35fccc121a2017fe582e5c771a6c444bcb7472a9
Anton Batalin <antbat2009@gmail.com>
2018-07-13 17:50:25 +0300

    add listener for heroku

--- bin/app.js
+++ bin/app.js
@@ -4,5 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Business_1 = __importDefault(require("./Business"));
+const express = require('express');
+express().listen(process.env.PORT || 3000);
 const business = new Business_1.default();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,6 @@
 import Business from './Business';
+const express = require('express');
+
+express().listen(process.env.PORT || 3000);
+
 const business = new Business();
